b9ca2075b2d5ca63457217ef4f75202a83f57e07
Owais Ahmad <146345893+owais2004@users.noreply.github.com>
2024-05-08 16:24:10 +0500

    Add files via upload

---
+++ OOP-Practice Set/ch1_PracticeSet.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CWH_Chapter1_PracticeSet {
+    public static void main(String[] args) {
+        //First Question
+        //Adding three numbers
+//        int a = 10;
+//        int b = 20;
+//        int c = 30;
+//        int sum = a + b + c;
+//        System.out.println(sum);
+        //Second Question
+        //Calculating the cgpa
+//        float subject1 = 45;
+//        float subject2 = 95;
+//        float subject3 = 48;
+//        float cgpa = (subject1 + subject2 + subject3)/30;
+//        System.out.println(cgpa);
+        //Third Question
+        //String concatenation
+//        System.out.println("What is your name ?");
+//        Scanner s1 = new Scanner(System.in);
+//        String name = s1.nextLine();
+//        System.out.println("My name is " + name);
+        //Fourth Question
+        //Converting kilometers to miles
+//        Scanner s1 = new Scanner(System.in);
+//        System.out.print("Enter the number of kilometers: ");
+//        double Kilometers = s1.nextDouble();
+//        double miles = Kilometers * (0.621);
+//        System.out.print("The number of miles will be: " + miles);
+        //Fifth Question
+        //To check the entered number is int or not
+
+    }
+}
---
+++ OOP-Practice Set/ch2_Practiceset.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CWH_ch2_11_PracticeSet2 {
+    public static void main(String[] args) {
+      //Question 01
+//        float a = 7 / 4f * 9 / 2f;
+//        System.out.println(a);
+      //Question 02
+//          char grade = 'I';
+//        char gradeForFather = grade;
+//          grade -= 8;
+//          char gradeForFriend = grade;
+//        System.out.println(gradeForFriend);
+//        System.out.println(gradeForFather);
+       //Question 03
+//        Scanner s1 = new Scanner(System.in);
+//        System.out.println("Enter a Number: ");
+//        int number = s1.nextInt();
+//        System.out.println(number > 8);
+        //Question 04
+//        int a = 2;
+//        int v = 4;
+//        int s = 3;
+//        int u = 5;
+//        int expression  = (v * v - u * u) / 2 * a * s;
+//        System.out.println(expression);
+        //Question 05
+        int z = 7;
+        int a = 7 * 49 / 7 + 35 / 7;
+        System.out.println(a);
+    }
+}
